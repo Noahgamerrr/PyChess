d08b0a571b5551c72908054077bcf84a4ff57da3
Noahgamerrr <noah.arsic@gmx.at>
2023-02-06 17:41:18 +0100

    Added the chess-board

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((800, 750))
+screen.fill('mediumseagreen')
 pygame.display.set_caption('PyChess')
 clock = pygame.time.Clock()
 
@@ -11,5 +12,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    isWhite = True
+    for i in range(8):
+        for j in range(8):
+            plate = pygame.Surface((70, 70))
+            plate.fill('cornsilk' if isWhite else 'burlywood4')
+            isWhite = not isWhite
+            screen.blit(plate, (120 + 70 * i, 100 + 70 * j))
+        isWhite = not isWhite
     pygame.display.update()
     clock.tick(60)
