80259e4acd78a9e9866e9bce70b8531903ac6257
Noahgamerrr <noah.arsic@gmx.at>
2023-02-08 12:12:59 +0100

    Pawns can now be selected

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from objects import *
+from typing import Tuple
 from sys import exit
+from pathlib import Path
 
 pygame.init()
 screen = pygame.display.set_mode((800, 750))
@@ -8,7 +10,11 @@ screen.fill('mediumseagreen')
 pygame.display.set_caption('PyChess')
 clock = pygame.time.Clock()
 PIECE_SIDE = 70
+START_X = 120
+START_Y = 100
 pieces = []
+circles = []
+boardRectangle = pygame.Rect(START_X, START_Y, 8 * PIECE_SIDE, 8 * PIECE_SIDE)
 
 
 def load_board():
@@ -18,15 +24,36 @@ def load_board():
             plate = pygame.Surface((PIECE_SIDE, PIECE_SIDE))
             plate.fill('cornsilk' if is_white else 'burlywood4')
             is_white = not is_white
-            screen.blit(plate, (120 + PIECE_SIDE * i, 100 + PIECE_SIDE * j))
+            screen.blit(plate, (START_X + PIECE_SIDE * i, START_Y + PIECE_SIDE * j))
         is_white = not is_white
+
+
+def drawCircle(pos: Tuple[int, int]):
+    rel_path = '\\sprites\\others\\move_circle.png'
+    source_path = Path(__file__).resolve()
+    source_dir = source_path.parent
+    image = pygame.image.load(source_dir.__str__() + rel_path)
+    x, y = pos
+    x_real = START_X + PIECE_SIDE * x + (PIECE_SIDE - image.get_width()) / 2
+    y_real = START_Y + PIECE_SIDE * y + (PIECE_SIDE - image.get_height()) / 2
+    screen.blit(image, (x_real, y_real))
+
+
+def drawCircles():
+    for pos in circles:
+        drawCircle(pos)
+
+
+def set_circles(piece: Piece):
+    global circles
+    circles = piece.get_moves()
 
 
 def load_single_piece(piece: Piece):
     image = piece.getImage()
     x, y = piece.get_pos()
-    x_real = 120 + PIECE_SIDE * x + (PIECE_SIDE - image.get_width()) / 2
-    y_real = 100 + PIECE_SIDE * y + (PIECE_SIDE - image.get_height()) / 2
+    x_real = START_X + PIECE_SIDE * x + (PIECE_SIDE - image.get_width()) / 2
+    y_real = START_Y + PIECE_SIDE * y + (PIECE_SIDE - image.get_height()) / 2
     screen.blit(image, (x_real, y_real))
 
 
@@ -57,6 +84,17 @@ def init_pieces():
     pieces.append(Queen("white", (3, 7)))
 
 
+def get_position(pos: Tuple[int]) -> Tuple[int, int]:
+    x, y = pos
+    if boardRectangle.collidepoint((x, y)):
+        return (int((x - START_X) / PIECE_SIDE), int((y - START_Y) / PIECE_SIDE))
+    return (-1, -1)
+
+
+def get_piece_on_board(pos: Tuple[int]) -> Piece:
+    return next((piece for piece in pieces if piece.get_pos() == get_position(pos)), None)
+
+
 init_pieces()
 
 while True:
@@ -64,7 +102,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if (get_position(pygame.mouse.get_pos()) != (-1, -1)):
+                set_circles(get_piece_on_board(pygame.mouse.get_pos()))
     load_board()
     load_pieces()
+    drawCircles()
     pygame.display.update()
     clock.tick(60)
--- objects.py
+++ objects.py
@@ -1,11 +1,11 @@
 import pygame
-from abc import ABC
-from typing import Tuple
+from abc import ABC, abstractmethod
+from typing import Tuple, List
 from pathlib import Path
 
 
 class Piece(ABC):
-    def __init__(self, colour: str, pos: Tuple[int, int]):
+    def __init__(self, colour: str, pos: Tuple[int, int]) -> None:
         self.colour = colour
         self.pos = pos
 
@@ -25,29 +25,56 @@ class Piece(ABC):
         rel_path = '\\sprites\\{}\\{}.png'.format(self.colour, self.get_class_name())
         source_path = Path(__file__).resolve()
         source_dir = source_path.parent
-        piece = pygame.image.load(source_dir.__str__() + rel_path)
-        return pygame.transform.rotozoom(piece, 0, 0.45)
+        image = pygame.image.load(source_dir.__str__() + rel_path)
+        return pygame.transform.rotozoom(image, 0, 0.45)
+
+    @abstractmethod
+    def get_moves() -> List[int]:
+        pass
 
 
 class Pawn(Piece):
-    pass
+    def __init__(self, colour: str, pos: Tuple[int, int]) -> None:
+        Piece.__init__(self, colour, pos)
+        self.has_moved = False
+
+    def get_moves(self) -> List[int]:
+        moves = []
+        black_white = 1 if self.colour == "white" else -1
+        moves.append((self.pos[0], self.pos[1] - (1 * black_white)))
+        if not self.has_moved:
+            moves.append((self.pos[0], self.pos[1] - (2 * black_white)))
+        return moves
 
 
 class Knight(Piece):
-    pass
+    def get_moves():
+        pass
 
 
 class Bishop(Piece):
-    pass
+    def get_moves():
+        pass
 
 
 class Rook(Piece):
-    pass
+    def get_moves():
+        pass
 
 
 class King(Piece):
-    pass
+    def get_moves():
+        pass
 
 
 class Queen(Piece):
-    pass
+    def get_moves():
+        pass
+
+
+class Circle:
+    def __init__(self, pos) -> None:
+        self.pos = pos
+
+    def get_pos(self) -> Tuple[int, int]:
+        return self.pos
